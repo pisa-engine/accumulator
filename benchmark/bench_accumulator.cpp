f96a7fb9a09df184c7c3d39f1ddae153832555aa
Antonio Mallia <me@antoniomallia.it>
2020-03-22 18:08:00 +0100

    added bench for accumulate

--- benchmark/bench_accumulator.cpp
+++ benchmark/bench_accumulator.cpp
@@ -2,11 +2,12 @@
 #include "MaxBlock.hpp"
 #include "Paged.hpp"
 #include "Simple.hpp"
+#include <algorithm>
 #include <nanobench.h>
+#include <random>
 #include <string>
-
 template <typename Accumulator>
-void perf_clear(std::string const& name, size_t iterations)
+void bench_clear(std::string const& name, size_t iterations)
 {
     for (auto&& power: {16, 18, 20, 25}) {
         auto size = std::pow(2, power);
@@ -20,10 +21,46 @@ void perf_clear(std::string const& name, size_t iterations)
     }
 }
 
+template <typename Accumulator>
+void bench_accumulate(std::string const& name, size_t iterations)
+{
+    auto RandomNumberBetween = [](auto low, auto high) {
+        auto randomFunc = [distribution_ = std::uniform_int_distribution(low, high),
+                           random_engine_ = std::mt19937{std::random_device{}()}]() mutable {
+            return distribution_(random_engine_);
+        };
+        return randomFunc;
+    };
+    size_t power = 20;
+    size_t size = std::pow(2, power);
+    for (auto&& ratio: {1, 10, 50, 80, 100}) {
+        size_t elements_size = ratio * size / 100;
+        std::vector<size_t> elements(elements_size);
+        std::generate(
+            elements.begin(), elements.end(), RandomNumberBetween(size_t(0), size_t(size - 1)));
+        Accumulator accumulator(size);
+        ankerl::nanobench::Bench()
+            .minEpochIterations(iterations)
+            .run(
+                "Accumulate elements in " + name + " accumulator of size 2^{"
+                    + std::to_string(power) + "} and ratio " + std::to_string(ratio) + "%",
+                [&] {
+                    for (auto&& e: elements) {
+                        accumulator.accumulate(e, e);
+                    }
+                })
+            .doNotOptimizeAway(accumulator);
+    }
+}
+
 int main(int argc, char const* argv[])
 {
     using namespace accumulator;
-    perf_clear<Simple<size_t>>("Simple", 1'000);
-    perf_clear<Paged<size_t>>("Paged", 10'000'000);
-    perf_clear<MaxBlock<size_t>>("MaxBlock", 10'000'000);
+    // bench_clear<Simple<size_t>>("Simple", 1'000);
+    // bench_clear<Paged<size_t>>("Paged", 10'000'000);
+    // bench_clear<MaxBlock<size_t>>("MaxBlock", 10'000'000);
+
+    bench_accumulate<Simple<size_t>>("Simple", 500);
+    bench_accumulate<Paged<size_t>>("Paged", 500);
+    bench_accumulate<MaxBlock<size_t>>("MaxBlock", 500);
 }
